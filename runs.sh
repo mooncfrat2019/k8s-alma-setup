1d43e47c076acfdc2a75aca9f2ddc6c9b92ca19a
Igor Kolomoytsev <i.kolomoytsev@vk.team>
2025-10-11 00:14:19 +0300

    Udpate etcd role 10

--- runs.sh
+++ runs.sh
@@ -13,7 +13,11 @@ ansible-playbook -i inventory/hosts.yml site.yml -e '{
   "host_1": "89.208.208.109",
   "host_2": "89.208.228.250",
   "host_3": "89.208.228.216",
+  "in_ip_1": "10.0.3.97",
+  "in_ip_2": "10.0.3.248",
+  "in_ip_3": "10.0.3.105",
   "host_registry": "89.208.208.139",
   "skip_bundle_transfer": true,
   "need_restart": false
-}' --tags common
+}' --tags etcd
+
